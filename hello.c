1ac3877377b048727d86cc13abe224a380d7db5f
Othmane Lahmadi <otmanwacreal20765@gmail.com>
2024-02-08 00:34:47 +0100

    kernel param

--- hello.c
+++ hello.c
@@ -1,15 +1,26 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
+#include <linux/moduleparam.h>
+
 MODULE_LICENSE("GPL");
 
+int param=333;
+
+module_param(param,int,S_IRUSR|S_IWUSR);
+
+void display(void){
+printk(KERN_ALERT " param = %d",param);
+}
+
 static int hello_init(void){
-printk(KERN_ALERT "TEST :hello worl");
+printk(KERN_ALERT "hello world");
+display();
 return 0;
 }
 
 static void hello_exit(void){
-printk(KERN_ALERT "TEST : God by");
+printk(KERN_ALERT "God by");
 }
 
 module_init(hello_init);
